ef583760a15c1e18aec006db0f45463d38b153ed
GonzaloCerda <gonz.cerda@duocuc.cl>
2021-05-24 15:19:14 -0400

    DATABASES

--- TiendaRopa/settings.py
+++ TiendaRopa/settings.py
@@ -74,12 +74,19 @@ WSGI_APPLICATION = 'TiendaRopa.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+	DATABASES = {
+	   'default': {
+	       'ENGINE': 'django.db.backends.oracle',
+	       'NAME': '127.0.0.1:1521/xe',
+	       'USER':'CPRUEBA',
+	       'PASSWORD': 'PRUEBA',
+	       'TEST':{
+	           'USER':'default_test',
+	           'TBLSPACE':'default_test_tbls',
+	           'TBLSPACE_TMP':'default_test_tbls_tmp',
+	       },
+	   },
+	}
 
 
 # Password validation
